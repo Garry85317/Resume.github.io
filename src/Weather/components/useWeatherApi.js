01dfffad9f5ccd7b75667aef2b5a36ea63efd9cd
Garry <95728941+Garry85317@users.noreply.github.com>
2023-05-25 14:15:22 +0800

    Update Weather Api Token

--- src/Weather/components/useWeatherApi.js
+++ src/Weather/components/useWeatherApi.js
@@ -17,7 +17,7 @@ const fetchSunsetTime = (locationName) => {
     .replace(/\//g, "-");
 
   return fetch(
-    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=CWB-B1382EB8-4FBC-4BF2-A358-C47A78DA3834F&format=JSON&locationName=${locationName}&dataTime=${nowDate}`
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=CWB-B1382EB8-4FBC-4BF2-A358-C47A78DA3834&format=JSON&locationName=${locationName}&dataTime=${nowDate}`
   )
     .then((response) => response.json())
     .then((data) => {
